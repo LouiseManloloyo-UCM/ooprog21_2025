a36c27e7ac04a77177f662c85e543d038f52d963
LouiseManloloyo-UCM <manloloyolouise8@gmail.com>
2025-08-28 21:33:38 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        
+        /*
+         Multi-line comments can be used to temporarily disable code
+         or to explain logic in more detail.
+         H
+         A
+         L
+         L
+         O
+        */
+
+        /**
+         * Documentation comments (Javadoc) are usually written
+         * above classes, methods, or variables to describe their purpose.
+         * They can be used by the `javadoc` tool to generate HTML docs.
+         */
+    }
+}
