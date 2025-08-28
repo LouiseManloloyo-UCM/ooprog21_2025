dcb739dfc7d642fad047ab5bf41ec3a03d84aa00
LouiseManloloyo-UCM <manloloyolouise8@gmail.com>
2025-08-28 21:06:07 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("You know I'm just a flight away");
+        System.out.println("If you want it, you can take a private plane");
+        System.out.println("A kilómetros estamos conectando");
+        System.out.println("Y me prendes aunque no me estés tocando");
+    }
+}
