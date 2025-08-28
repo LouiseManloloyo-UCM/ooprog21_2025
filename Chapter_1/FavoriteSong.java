8a8dc3d20a74e2d592503644c5dd0e5efeff4f06
LouiseManloloyo-UCM <manloloyolouise8@gmail.com>
2025-08-28 21:14:56 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -4,5 +4,10 @@ public class FavoriteSong {
         System.out.println("If you want it, you can take a private plane");
         System.out.println("A kilómetros estamos conectando");
         System.out.println("Y me prendes aunque no me estés tocando");
+        System.out.println("You know I got a lot to say");
+        System.out.println("All these voices in the background of my brain");
+        System.out.println("Y me dicen todo lo que estás pensando");
+        System.out.println("Me imagino lo que ya estás maquinando");
     }
 }
+
