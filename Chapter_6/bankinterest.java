7e08f65635b34e87e233d3eac4cd61d815ac814a
LouiseManloloyo-UCM <manloloyolouise8@gmail.com>
2025-10-27 10:37:57 +0800

    Add files via upload

---
+++ Chapter_6/bankinterest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class bankinterest {
+ public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+double bal;
+System.out.print("enter initial bank balance > ");
+ bal = sc.nextDouble();
+        
+double rate[] = {0.02,0.03,0.04,0.05};
+ double newbal;
+
+for(int i=0;i<rate.length;i++){
+newbal=bal;
+System.out.println("\nwith an initial balance of $" + bal + " at an interest rate of " + rate[i]);
+for(int y=1;y<=4;y++){
+newbal=newbal+(newbal*rate[i]);
+System.out.println("after year " + y + " balance is $" + newbal);
+            }
+        }
+sc.close();
+System.out.println("\n----jgrasp: operation complete.");
+    }
+}
